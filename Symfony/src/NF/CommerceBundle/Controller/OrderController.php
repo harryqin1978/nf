b5d5794c0006167199a0b3fc5f7bc951c461a72f
Harry Qin <zergling9999@hotmail.com>
2014-07-11 15:49:11 +0800

    commerce order list basic

--- Symfony/src/NF/CommerceBundle/Controller/OrderController.php
+++ Symfony/src/NF/CommerceBundle/Controller/OrderController.php
@@ -14,8 +14,32 @@ class OrderController extends Controller
      */
     public function indexAction()
     {
+
+        $page_size = $this->container->getParameter('nf_page_size');
+        $page_num = (int) $this->get('request')->query->get('page', 1);
+        $page_firstrecord = ($page_num - 1) * $page_size;
+
+        $em = $this->getDoctrine()->getManager();
+
+        $page_totalcount = $em
+            ->createQuery("SELECT COUNT(c.id) AS result FROM NFCommerceBundle:Order c")
+            ->getSingleResult();
+        $page_totalcount = $page_totalcount['result'];
+        $page_count = ceil($page_totalcount / $page_size);
+
+        $entities = $em
+            ->getRepository('NFCommerceBundle:Order')
+            ->createQueryBuilder('c')
+            ->setFirstResult($page_firstrecord)
+            ->setMaxResults($page_size)
+            ->getQuery()
+            ->getResult();
+
         return array(
-            'page_title' => 'nf.commerce.page.title.orders'
+            'page_title' => 'nf.commerce.page.title.orders',
+            'entities' => $entities,
+            'page_num' => $page_num,
+            'page_count' => $page_count,
         );
     }
 
